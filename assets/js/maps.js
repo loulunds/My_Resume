ececcfcbaa01ac93f1845759d2caa33ae5b4e77b
loulunds <lousanne.lundstrom@gmail.com>
2020-02-06 22:44:54 +0000

    github page

---
+++ assets/js/maps.js
@@ -0,0 +1,37 @@
+function initMap () {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom : 3,
+        center: {
+            lat: 59.324862,
+            lng: 18.071113
+        }
+    });
+
+var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+var locations = [{
+        lat: 59.334034 , 
+        lng: 18.066329
+    }, {
+        lat: 59.337103 , 
+        lng: 18.062611
+    }, {
+        lat: 59.402372 , 
+        lng: 17.944621
+    }];
+
+
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+
+
+    var markerCluster = new MarkerClusterer(map, markers,{
+        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+        });
+
+}
